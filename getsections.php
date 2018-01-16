747a9a473136786b15580ca9abe3a5a31341a63c
KhaldoonAlkrad <you@example.com>
2018-01-16 15:09:31 +0100

    01-16-2018 15:09

--- getsections.php
+++ getsections.php
@@ -9,11 +9,11 @@ for ($x = 0; $x < count($sectionArray); $x++) {
     echo "<div class=section> ";
     echo "<table>";
     echo "<tr>";
-    echo "<td rowspan=2>".$sectionArray[$x]->letter . "</td>";
-    echo "<td>".$sectionArray[$x]->name . "</td>";
+    echo "<td rowspan=2>" . $sectionArray[$x]->letter . "</td>";
+    echo "<td>" . $sectionArray[$x]->name . "</td>";
     echo "</tr>";
     echo "<tr>";
-    echo "<td>".$sectionArray[$x]->content . "</td>";
+    echo "<td>" . $sectionArray[$x]->content . "</td>";
     echo "</tr>";
     echo "</table>";
     echo '</div>';
--- viewcourse.php
+++ viewcourse.php
@@ -16,12 +16,8 @@ if (isset($_SESSION["name"]) && $_SESSION["name"] != "") {
     <div id="sidelist-container">
         <?php course::showcourselist(); ?>
     </div>
-    <div id="lesson-title">
-            
-        </div>
-    <div id="data-container">
-        
-        
-
+    <div id="lesson-container">
+        <div id="lesson-title"></div>
+        <div id="data-container"></div>
     </div>
 </div>
