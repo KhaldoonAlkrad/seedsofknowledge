608bda8be922e2d27e14c86a402fbb137dbb676b
KhaldoonAlkrad <you@example.com>
2018-01-17 16:19:30 +0100

    01-17-2018 16:19

--- include/templates/loginnav.php
+++ include/templates/loginnav.php
@@ -4,20 +4,21 @@
         <a href="makecourse.php"><span class="glyphicon glyphicon-list-alt " ></span> Course &nbsp;</a>
         <a href="subcourse.php"><span class="glyphicon glyphicon-list-alt " ></span> Subcourse &nbsp;</a>
         <a href="lesson.php"><span class="glyphicon glyphicon-list-alt " ></span> Lesson &nbsp;</a>
-         <a href="section.php"><span class="glyphicon glyphicon-list-alt " ></span> Section &nbsp;</a>
+        <a href="section.php"><span class="glyphicon glyphicon-list-alt " ></span> Section &nbsp;</a>
         <a href="viewcourse.php"><span class="glyphicon glyphicon-eye-open" ></span> View Course &nbsp;</a>
         <a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Log out&nbsp;</a>
-        
+
         <a href="#"><span class="glyphicon glyphicon-user" ></span> 
-            <?php 
-          
-            if(isset($_SESSION["name"]) && $_SESSION["name"]!="" )
-                echo $_SESSION["name"];?>
+            <?php
+            if (isset($_SESSION["name"]) && $_SESSION["name"] != "")
+                echo $_SESSION["name"];
+            ?>
             &nbsp;</a>
 
     </nav>
-    <header class="jumbotron text-center">
-        <h1 id="mainheader">Seeds of Knowledge</h1>
+    <header class="jumbotron text-center headercontainer">
+        <!--        <h1 id="mainheader">Seeds of Knowledge</h1>-->
+        <img id="headerimg" src="<?php echo $img; ?>seeds-of-knowledge.png" alt="Header Image">
     </header>
 
 </div> 
